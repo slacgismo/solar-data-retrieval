e03404f36845276940f544d846a24a8be605d224
elpiniki <elpiniki.apostolaki@gmail.com>
2019-07-29 14:25:06 -0700

    push changes

--- solardataretrieval/utilities.py
+++ solardataretrieval/utilities.py
@@ -16,6 +16,7 @@ with open(home + '/.aws/credentials') as f:
         secret = lns[4].split(' = ')[1].strip('\n')
 
 import s3fs
+
 #this function returns all IDs of solar installations that exist in SunPower Cassandra database
 #the path of cassandra_cluster should be a string, example '/Users/elpiniki/.aws/cassandra_cluster'
 #the output is a list of IDs as strings
@@ -65,12 +66,12 @@ def get_data_from_Cassandra(sites_selected, cassandra_cluster_path):
 #this function preprocess the data and normlize them based on a quantile
 #this function retuns two datafiles: power signals and a list of qualtile value per site
 def data_pipeline_process(data_input, site_IDs, sensor_IDs,quantile_percent,number_of_days, sorted_days):
+    power_signals_selected_days_all = []
     site_errors = 0
     quantile_values = []
     sensor_IDs_output = []
     side_IDs_output = []
     for i in range(0,len(sensor_IDs)):
-        print(sensor_IDs[i])
         try:
             df_sensor = data_input.loc[data_input['sensor'] == sensor_IDs[i]]
             df_sensor.replace(-999999.0, np.NaN, inplace=True)
@@ -96,38 +97,26 @@ def data_pipeline_process(data_input, site_IDs, sensor_IDs,quantile_percent,numb
             side_IDs_output.append(site_IDs[i])
             if sorted_days is True:
                 list_of_selected_dates.sort()
-            power_signals_selected_days = power_signals[:,list_of_selected_dates]
-            if sensor_IDs[i] == sensor_IDs[0]:
-                power_signals_selected_days_all = power_signals_selected_days
+            if np.quantile(power_signals_1, quantile_percent) > 0.0 or np.quantile(power_signals_1, quantile_percent) != np.nan:
+                power_signals_selected_days = power_signals[:,list_of_selected_dates]
+                if sensor_IDs[i] == sensor_IDs[0]:
+                    power_signals_selected_days_all = power_signals_selected_days
+                else:
+                    power_signals_selected_days = power_signals_selected_days
+                    try:
+                        power_signals_selected_days_all = np.concatenate([power_signals_selected_days_all, power_signals_selected_days], axis=1)
+                    except:
+                        site_errors = site_errors+1
+                        quantile_values.remove(quantile_values[len(quantile_values)-1])
+                        sensor_IDs_output.remove(sensor_IDs_output[len(sensor_IDs_output)-1])
+                        side_IDs_output.remove(side_IDs_output[len(side_IDs_output)-1])
+                        pass
             else:
-                power_signals_selected_days = power_signals_selected_days
-                try:
-                    power_signals_selected_days_all = np.concatenate([power_signals_selected_days_all, power_signals_selected_days], axis=1)
-                except:
-                    site_errors = site_errors+1
-                    print ("error {}".format(sensor_id))
-                    print ("remove")
-                    print (quantile_values[len(quantile_values)-1])
-                    quantile_values.remove(quantile_values[len(quantile_values)-1])
-                    sensor_IDs_output.remove(sensor_IDs_output[len(sensor_IDs_output)-1])
-                    side_IDs_output.remove(side_IDs_output[len(side_IDs_output)-1])
-                    pass
+                pass
         except:
             pass
     return power_signals_selected_days_all, quantile_values, site_errors, sensor_IDs_output, side_IDs_output
 
-def reconstract_data_pipeline(data_input, quantiles_per_site, number_of_days, number_of_sites):
-    number_of_sites_index = np.arange(0, number_of_sites)
-    start_index = np.multiply(number_of_sites_index, number_of_days)
-    for s in range(0,number_of_sites):
-        end_index = start_index[s]+number_of_days
-        reconstracted_data = np.multiply(data_input[:, start_index[s]:end_index], quantiles_per_site[s])
-        if s ==0:
-            reconstructed_data_all = reconstracted_data
-        else:
-            reconstracted_data = reconstracted_data
-            reconstructed_data_all = np.concatenate([reconstructed_data_all, reconstracted_data], axis=1)
-    return reconstructed_data_all
 
 def AWS_upload(data_to_upload_df,number_of_sites, site_errors, number_of_days, kind):
     #upload to aws
